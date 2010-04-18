c5fd3cea40d1f063c51497b746998b503580b34a
anton@chirkunov.com <anton@acmbp.local>
2010-04-18 22:10:36 +0400

    The spec will now properly use activerecord 3.0.0.beta3 and arel

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,11 +1,15 @@
 require 'rubygems'
 require 'spec'
-gem 'activerecord', '=2.3.5'
+gem 'activerecord', '=3.0.0.beta3'
+#require 'active_record'
+require 'arel'
 
 $:.unshift((File.join(File.dirname(__FILE__), '..', 'lib')))
 require 'spatial_adapter'
 
 include GeoRuby::SimpleFeatures
+
+Arel::Table.engine = Arel::Sql::Engine.new(ActiveRecord::Base)
 
 # Don't output migration logging
 ActiveRecord::Migration.verbose = false
